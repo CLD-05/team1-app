3081d45413fec4c0e028c126062b414e85b61a93
jotace06 <jjch422@naver.com>
2026-05-24 08:06:03 +0900

    chore: 프로젝트 초기 세팅 및 실행 가능한 최소한의 설정

---
+++ src/test/java/com/ops/app/courseregistration/Team1AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ops.app.courseregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
