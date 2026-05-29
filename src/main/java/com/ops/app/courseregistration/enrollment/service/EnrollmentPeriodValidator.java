83836aa761ba0f7c8c2cf60655936ca885e25af2
edbdms <edbdms@naver.com>
2026-05-29 13:58:11 +0900

    feat: 테스트를 위한 오픈 시간 변경 (#34)

--- src/main/java/com/ops/app/courseregistration/enrollment/service/EnrollmentPeriodValidator.java
+++ src/main/java/com/ops/app/courseregistration/enrollment/service/EnrollmentPeriodValidator.java
@@ -12,14 +12,15 @@ public class EnrollmentPeriodValidator {
 
     // 수강신청 가능 시간대
     private static final LocalTime[][] PERIODS = {
-            { LocalTime.of(10, 0),  LocalTime.of(10, 30) },
+            { LocalTime.of(9, 30),  LocalTime.of(19, 0) }
+           /* { LocalTime.of(10, 0),  LocalTime.of(10, 30) },
             { LocalTime.of(14, 0),  LocalTime.of(14, 30) },
-            { LocalTime.of(16, 0),  LocalTime.of(16, 30) }
+            { LocalTime.of(16, 0),  LocalTime.of(16, 30) }*/
     };
 
     // 서버 운영 시간
     private static final LocalTime SERVER_OPEN  = LocalTime.of(9, 30);
-    private static final LocalTime SERVER_CLOSE = LocalTime.of(16, 30);
+    private static final LocalTime SERVER_CLOSE = LocalTime.of(19, 0);
 
     public void validate() {
         LocalTime now = LocalTime.now();
